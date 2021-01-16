3fece606fd6357c229b9609b306b3f7a8fa313fc
PJEilers <p.j.eilers@student.rug.nl>
2021-01-16 15:28:02 +0100

    Changed calculations from TES to TCS

--- shaders/pnquadEvaluationShader.glsl
+++ shaders/pnquadEvaluationShader.glsl
@@ -2,8 +2,8 @@
 
 layout(quads, equal_spacing, ccw) in;
 
-layout (location = 0) in vec3 tess_vertcoords_camera_fs[];
-layout (location = 1) in vec3 tess_vertnormal_camera_fs[];
+layout (location = 0) patch in vec3 cp [16];
+layout (location = 16) patch in vec3 np [9];
 
 uniform mat4 modelviewmatrix;
 uniform mat4 projectionmatrix;
@@ -14,6 +14,7 @@ uniform float sigma;
 
 layout (location = 0) out vec3 vertcoords_camera_fs;
 layout (location = 1) out vec3 vertnormal_camera_fs;
+
 
 
 
@@ -36,86 +37,33 @@ float b_3(float t) {
 
 void main() {
 
-    //  b0  - b03 - b30 - b3
-    //  |     |     |     |
-    //  b01 - b02 - b31 - p32
-    //  |     |     |     |
-    //  b10 - b13 - b20 - p23
-    //  |     |     |     |
-    //  b1 -  b12 - b21 - b2
 
-    // Maps to: (where p0 is array[0] etc.)
+    vec3 b0 = cp[0];
+    vec3 b1 = cp[1];
+    vec3 b2 = cp[2];
+    vec3 b3 = cp[3];
+    vec3 b01 = cp[4];
+    vec3 b10 = cp[5];
+    vec3 b12 = cp[6];
+    vec3 b21 = cp[7];
+    vec3 b23 = cp[8];
+    vec3 b32 = cp[9];
+    vec3 b30 = cp[10];
+    vec3 b03 = cp[11];
+    vec3 b02 = cp[12];
+    vec3 b13 = cp[13];
+    vec3 b20 = cp[14];
+    vec3 b31 = cp[15];
 
-    //  p0 -  p4 - p8 - p12
-    //  |     |     |     |
-    //  p1 -  p5 - p9 - p13
-    //  |     |     |     |
-    //  p2 -  p6 - p10 - p14
-    //  |     |     |     |
-    //  p3 -  p7 - p11 - p15
-
-
-    vec3 p0 = gl_in[2].gl_Position.xyz;
-    vec3 p1 = gl_in[3].gl_Position.xyz;
-    vec3 p2 = gl_in[0].gl_Position.xyz;
-    vec3 p3 = gl_in[1].gl_Position.xyz;
-
-    vec3 n0 = normalize(tess_vertnormal_camera_fs[2]);
-    vec3 n1 = normalize(tess_vertnormal_camera_fs[3]);
-    vec3 n2 = normalize(tess_vertnormal_camera_fs[0]);
-    vec3 n3 = normalize(tess_vertnormal_camera_fs[1]);
-
-    // Compute boundary points
-
-    vec3 b0  = p0;
-    vec3 b01 = (2.0 * p0 + p1 - dot(p1 - p0, n0) * n0) / 3.0;
-    vec3 b10 = (2.0 * p1 + p0 - dot(p0 - p1, n1) * n1) / 3.0;
-
-    vec3 b1 = p1;
-    vec3 b12 = (2.0 * p1 + p2 - dot(p2 - p1, n1) * n1) / 3.0;
-    vec3 b21 = (2.0 * p2 + p1 - dot(p1 - p2, n2) * n2) / 3.0;
-
-    vec3 b2 = p2;
-    vec3 b23 = (2.0 * p2 + p3 - dot(p3 - p2, n2) * n2) / 3.0;
-    vec3 b32 = (2.0 * p3 + p2 - dot(p2 - p3, n3) * n3) / 3.0;
-
-    vec3 b3 = p3;
-    vec3 b30 = (2.0 * p3 + p0 - dot(p0 - p3, n3) * n3) / 3.0;
-    vec3 b03 = (2.0 * p0 + p3 - dot(p3 - p0, n0) * n0) / 3.0;
-
-    vec3 q = b01 + b10 + b12 + b21 + b23 + b32 + b30 + b03;
-
-    //b02
-    vec3 e = (2.0 * (b01 + b03 + q) - (b21 + b23)) / 18.0;
-    vec3 v = (4.0 * p0 + 2.0 * (p3 + p1) + p2) / 9.0;
-    vec3 b02 = (1.0 + sigma) * e - sigma * v;
-
-    //b13
-    e = (2.0 * (b12 + b10 + q) - (b32 + b30)) / 18.0;
-    v = (4.0 * p1 + 2.0 * (p0 + p2) + p3) / 9.0;
-    vec3 b13 = (1.0 + sigma) * e - sigma * v;
-
-    //b20
-    e = (2.0 * (b23 + b21 + q) - (b03 + b01)) / 18.0;
-    v = (4.0 * p2 + 2.0 * (p1 + p3) + p0) / 9.0;
-    vec3 b20 = (1.0 + sigma) * e - sigma * v;
-
-    //b31
-    e = (2.0 * (b30 + b32 + q) - (b10 + b12)) / 18.0;
-    v = (4.0 * p3 + 2.0 * (p2 + p0) + p1) / 9.0;
-    vec3 b31 = (1.0 + sigma) * e - sigma * v;
-
-    float v01 = (2 * (dot(p1 - p0, n0 + n1) / dot(p1 - p0, p1 - p0)));
-    float v12 = (2 * (dot(p2 - p1, n1 + n2) / dot(p2 - p1, p2 - p1)));
-    float v23 = (2 * (dot(p3 - p2, n2 + n3) / dot(p3 - p2, p3 - p2)));
-    float v30 = (2 * (dot(p0 - p3, n3 + n0) / dot(p0 - p3, p0 - p3)));
-
-    vec3 n01 = normalize(n0 + n1 - v01 * (p1 - p0));
-    vec3 n12 = normalize(n1 + n2 - v12 * (p2 - p1));
-    vec3 n23 = normalize(n2 + n3 - v23 * (p3 - p2));
-    vec3 n30 = normalize(n3 + n0 - v30 * (p0 - p3));
-
-    vec3 n0123 = ((2.*(n01 + n12 + n23 + n30)) + (n0 + n1 + n2 + n3)) / 12.;
+    vec3 n0 = np[0];
+    vec3 n1 = np[1];
+    vec3 n2 = np[2];
+    vec3 n3 = np[3];
+    vec3 n01 = np[4];
+    vec3 n12 = np[5];
+    vec3 n23 = np[6];
+    vec3 n30 = np[7];
+    vec3 n0123 = np[8];
 
     float su = gl_TessCoord.x;
     float sv = gl_TessCoord.y;
